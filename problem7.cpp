ebae51e63fd70115e4e9cb9da2f13217aa03e0ed
AlijonTursunboyev <147589319+AlijonTursunboyev@users.noreply.github.com>
2024-04-03 14:17:49 +0500

    Create problem7.cpp

---
+++ problem7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string SubString(const string& str, int i, int j) {
+  
+  if (i < 0 || j < 0 || i >= str.length() || j <= i) {
+    return ""; 
+  }
+  if (i == j) {
+    return string(1, str[i]); 
+  }
+  return str[i] + SubString(str, i + 1, j);
+}
+
+int main() {
+  string str;
+  cin>>str;
+  int i,j;
+  cin >>i;
+  cin>>j;
+  
+ string subString = SubString(str, i, j);
+  
+ cout << "Substring: " << subString << endl; 
+  
+  return 0;
+}
